32868da52c4fafddb1191fcb6b39873b822ed0b9
thelaagy <laggymaya@gmail.com>
2026-04-11 19:04:46 +0000

    Criacao do componente Button e sua logica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+import Button from "@/components/ui/Button"
+import React from "react"
+
 export default function Home() {
-  return <h1>Five Nights at Luiz's</h1>
+
+
+    return <>
+        <h1>Five Nights at Luiz's</h1>
+    </>
 }
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import '@/app/globals.css'
+
+
+interface Props {
+    label: string;
+    type?: "submit" | "reset" | "button" | undefined;
+    variant?: "primary" | "secondary" | "tertiary";
+    onClick?: React.MouseEventHandler;
+}
+// Colocar 'use client'; no começo dos arquivos que vão usar o botão
+
+export default function Button({ label, variant = "primary", type, onClick }: Props) {
+    const styles = "px-4 py-2 rounded-md fount-medium transition all"
+    const variants = {
+        primary: "bg-blue-500 text-white hover:bg-blue-600",
+        secondary: "bg-gray-500 text-white hover:bg-gray-600",
+        tertiary: "bg-red-500 text-white hover:bg-red-600",
+    };
+
+    return <button onClick={onClick} type={type} className={`${styles} ${variants[variant]}`}>{label}</button>;
+}
