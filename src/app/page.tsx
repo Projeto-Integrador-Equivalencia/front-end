7c78386e2ba70605c5106b357e0833c7cc6397d9
LeoCPassos <leo-cpassos@hotmail.com>
2026-04-13 00:20:46 -0300

    restaurar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-'use client';
-import Button from "@/components/ui/Button"
-import React from "react"
-
 export default function Home() {
-
-
-    return <>
-        <h1>Five Nights at Luiz's</h1>
-    </>
+  return <h1>Five Nights at Luiz's</h1>
 }
