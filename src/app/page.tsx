a70ca588edc3690cd044c2671163e604cc97237a
LeoCPassos <leo-cpassos@hotmail.com>
2026-04-13 00:29:33 -0300

    page.txt fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {    
-    return <h1>Five Nights at Luiz's</h1>
+import React from "react";
+
+export default function Home() {
+  return( 
+  <html lang="pt-br">
+      <body style={{ margin: 0, padding: 0 }}>
+        <main style={{ flex: 1 }}>
+        <h1>Hello World !!!!</h1>
+        </main>
+      </body>
+    </html>
+
+  )
 }
