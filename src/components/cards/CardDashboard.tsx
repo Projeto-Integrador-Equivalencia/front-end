a64b73eeba03f7181d44d74ef8faec0a569d8599
LuizHLimaS <luiz.durique@gmail.com>
2026-04-19 20:28:00 +0000

    adiciona componentes de card e de titulo

--- src/components/cards/CardDashboard.tsx
+++ src/components/cards/CardDashboard.tsx
@@ -8,7 +8,7 @@ export default function CardDashboard({
   children
 }: Props) {
   return (
-      <Card className="max-w-[460px] max-h-[672px] w-[27%] h-[70%] place-items-center">
+      <Card className="max-w-[460px] max-h-[672px] mx-auto w-[27%] h-[70%] place-items-center">
        <div className="mt-[70px]">
         {children}
        </div>
---
+++ src/components/cards/CardForm.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "../ui/Card";
+
+type Props = {
+} & React.HTMLAttributes<HTMLElement>;
+
+export default function CardForm({
+    children, 
+    onSubmit 
+}: Props) {
+  return (
+    <div className="bg-white rounded-2xl flex overflow-hidden mx-auto w-screen sm:w-[80%] h-[650px] shadow relative ">
+      {/* Lado Esquerdo */}
+      <div className="w-0 sm:w-1/2">
+        <img
+          src="/images/ciemIII.jpeg"
+          alt="Foto Fatec Atibaia"
+          className="h-full w-full object-cover"
+        />
+      </div>
+
+      {/* Lado Direito */}
+      <div className="sm:w-1/2 p-1 mx-auto">
+        <form onSubmit={onSubmit} className="items-center p-1.5 sm:p-6">
+          {children}
+        </form>
+      </div>
+    </div>
+  );
+}
