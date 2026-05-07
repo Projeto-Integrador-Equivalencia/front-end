6523988d4af9707c7c9ce3e65b10178aeb6f26d8
LuizHLimaS <luiz.durique@gmail.com>
2026-05-07 02:42:16 +0000

    Correcao nos componentes de background

--- src/components/backgrounds/WhiteRedBackground.tsx
+++ src/components/backgrounds/WhiteRedBackground.tsx
@@ -3,10 +3,12 @@ import Background from "../ui/Background";
 
 type Props = {} & React.HTMLAttributes<HTMLElement>;
 
-export default function BackgroundWhiteRed({ children }: Props) {
+export default function BackgroundWhiteRed({ children, className }: Props) {
   return (
     <Background>
-      <div className="absolute inset-0 -z-10 flex flex-col bg-[linear-gradient(to_bottom,white_50%,#cc0000_50%)]" />
+      <div
+        className={`absolute inset-0 -z-10 flex flex-col bg-[linear-gradient(to_bottom,white_50%,#cc0000_50%)] ${className}`}
+      />
       {children}
     </Background>
   );
