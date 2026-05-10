c5f787f4cbbc3cde4b6c8bc01e7f4d1c6ef10189
Lucas Barbosa <lucasesbarbosa@gmail.com>
2026-05-10 16:55:23 +0000

    Corrigido header para nao sobrepor corpo da pagina

--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="w-full absolute top-0 left-0 h-20 bg-black  flex items-center justify-center">
+        <header className="fixed w-full absolute top-0 left-0 h-20 bg-black  flex items-center justify-center">
             <div className="relative w-full max-w-400 flex items-center justify-center">
                 <div className="flex flex-col items-center">
                     <Image
