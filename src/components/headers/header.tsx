0f1f0bdbc65f04aa9c36f0028c135de72512c009
Lucas Barbosa <lucasesbarbosa@gmail.com>
2026-06-08 01:32:47 +0000

    adicionado dropdown menu e funcionanlidade no header

--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -1,10 +1,37 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import DropdownMenu from "./DropdownMenu";
+import { useAuth } from "@/hooks/userAuth"; // Ajuste este caminho se necessário
 
 export default function Header() {
+    const { user, signOut } = useAuth();
+    const router = useRouter();
+
+    const nomeUsuario = user?.name;
+
+    const opcoesDoUsuario = [
+        { 
+            label: 'Sair', 
+            action: () => {
+                signOut(); 
+                router.push("/"); 
+            } 
+        }
+    ];
+
     return (
-        <header className="sticky w-full absolute top-0 left-0 h-20 bg-black flex items-center justify-center z-50">
-            <div className="relative w-full max-w-300 flex items-center justify-center">
-                <div className="flex flex-col items-center">
+   
+        <header className="sticky top-0 left-0 h-20 bg-black text-white w-full flex items-center justify-between px-8 z-50">
+            
+
+            <div className="flex-1">
+            </div>
+
+            <div className="flex-1 flex justify-center">
+                <Link href="/">
                     <Image
                         src="/images/logo-fatec.png"
                         alt="Logo Fatec"
@@ -12,8 +39,19 @@ export default function Header() {
                         height={50}
                         priority
                     />
-                </div>
+                </Link>
+                
             </div>
+
+            <div className="flex-1 flex justify-end">
+                {nomeUsuario && (
+                    <DropdownMenu
+                        buttonText={nomeUsuario}
+                        items={opcoesDoUsuario}
+                    />
+                )}
+            </div>
+            
         </header>
     );
 }
