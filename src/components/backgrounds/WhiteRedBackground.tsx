56218f9274fec72ffd833bac879f9ce4153411a6
Lucas Barbosa <lucasesbarbosa@gmail.com>
2026-06-17 08:04:07 +0000

    ajustado responsividade da tela de solicitacao de equivalencias

--- src/components/backgrounds/WhiteRedBackground.tsx
+++ src/components/backgrounds/WhiteRedBackground.tsx
@@ -9,7 +9,7 @@ export default function BackgroundWhiteRed({ children, className }: Props) {
       <div
         className={`absolute bottom-0 left-0 z-0 w-full h-3/4 rounded-xl md:h-1/2 md:rounded-none bg-[#cc0000] ${className}`}
       />
-      <div className="relative z-10 flex-1 flex flex-col w-full h-full">
+      <div className="relative z-10 flex-1 flex flex-col w-full min-h-full">
         {children}
       </div>
     </Background>
--- src/components/cards/CardBalckWhite.tsx
+++ src/components/cards/CardBalckWhite.tsx
@@ -5,24 +5,28 @@ interface CardBlackWhiteProps {
 
 export default function CardBlackWhiteProps({ leftContent, rightContent }: CardBlackWhiteProps) {
   return (
-    <div className="bg-white rounded-2xl flex overflow-hidden mx-auto w-full max-w-275 min-h-162.5 shadow-xl relative">
+    <div className="flex flex-col space-y-15 md:flex-row bg-white rounded-2xl overflow-hidden mx-auto w-full max-w-275 min-h-162.5 shadow-xl relative">
       
-      <div className="hidden sm:flex sm:w-[40%] bg-zinc-950 p-8 flex-col justify-center items-center relative">
+      <div className="flex sm:w-[40%] bg-zinc-950 p-8 flex-col justify-center items-center relative">
         {leftContent}
         
-        <img 
-          src="/images/PontosBL.png" 
-          className="absolute bottom-4 left-4 w-12 opacity-20 pointer-events-none" 
-        />
+        <div className="hidden sm:flex">
+         <img 
+           src="/images/PontosBL.png" 
+           className="absolute bottom-4 left-4 w-12 opacity-20 pointer-events-none" 
+          />
+        </div>
       </div>
 
       <div className="w-full sm:w-[60%] p-8 sm:p-16 flex flex-col justify-center relative">
         {rightContent}
 
-        <img 
-          src="/images/PontosBR.png" 
-          className="absolute bottom-4 right-4 w-12 opacity-20 pointer-events-none" 
-        />
+        <div className="hidden sm:flex">
+          <img 
+            src="/images/PontosBR.png" 
+            className="absolute bottom-4 right-4 w-12 opacity-20 pointer-events-none" 
+          />
+        </div>
       </div>
     </div>
   );
