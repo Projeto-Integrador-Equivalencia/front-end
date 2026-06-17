de78e79bbd0d7c4856affbae7b58a63e2e439822
Lucas Barbosa <lucasesbarbosa@gmail.com>
2026-06-17 08:20:18 +0000

    Ajustado responsividade da tela CoursesList

--- src/components/cards/CardWhite.tsx
+++ src/components/cards/CardWhite.tsx
@@ -6,7 +6,7 @@ type Props = {} & React.HTMLAttributes<HTMLElement>;
 export default function CardWhite({ children, className }: Props) {
   return (
     <Card
-      className={`rounded-2xl flex w-full max-w-6xl aspect-video overflow-auto ${className}`}
+      className={`rounded-2xl flex w-full max-w-6xl md:aspect-video overflow-auto overflow-x-auto ${className}`}
     >
       <div className="p-1 mx-auto">{children}</div>
     </Card>
